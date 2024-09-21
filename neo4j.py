b99a7ff4595a1764f1693bb09e98836d9a55f82f
comjke33 <comjke33@inu.ac.kr>
2024-09-21 22:05:03 +0900

    fuyu 모델 테스트

---
+++ neo4j.py
@@ -0,0 +1,14 @@
+import os
+
+os.environ["OPENAI_API_KEY"] = "sk-"
+os.environ["NEO4J_URI"] = "neo4j+s://demo.neo4jlabs.com"
+os.environ["NEO4J_USERNAME"] = "companies"
+os.environ["NEO4J_PASSWORD"] = "companies"
+os.environ["NEO4J_DATABASE"] = "companies"
+
+embeddings = OpenAIEmbeddings()
+graph = Neo4jGraph()
+vector_index = Neo4jVector.from_existing_index(
+    embeddings, 
+    index_name="news"
+)
